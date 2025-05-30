76af84d134ae0ec93d367822b517c078078e7535
Patrick McCann <patmmccann@gmail.com>
2025-05-30 16:01:11 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -537,10 +537,6 @@ gulp.task('test-only', test);
 gulp.task('test-all-features-disabled', testTaskMaker({disableFeatures: require('./features.json'), oneBrowser: 'chrome', watch: false}));
 gulp.task('test', gulp.series(clean, lint, 'test-all-features-disabled', 'test-only'));
 
-gulp.task('test-coverage', gulp.series(clean, testCoverage));
-gulp.task(viewCoverage);
-
-
 // npm will by default use .gitignore, so create an .npmignore that is a copy of it except it includes "dist"
 gulp.task('setup-npmignore', shell.task("sed 's/^\\/\\?dist\\/\\?$//g;w .npmignore' .gitignore", {quiet: true}));
 gulp.task('build', gulp.series(clean, 'build-bundle-prod', updateCreativeExample, setupDist));
